882712b1242a5481edd82c4e66c7dce3fb64fe86
kootsZhin <69758390+kootsZhin@users.noreply.github.com>
2022-07-29 13:07:32 -0400

    fix hydration error

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,37 +1,47 @@
-import { useStarknet, useConnectors } from '@starknet-react/core'
+import { useConnectors, useStarknet } from '@starknet-react/core';
+import { useEffect, useState } from 'react';
 
 function shortenAddress(address: string) {
+  if (!address) return "";
   return address.substring(0, 4) + '...' + address.substring(address.length - 4)
 }
+
 
 export default function ConnectWallet() {
   const { account } = useStarknet()
   const { available, connect, disconnect } = useConnectors()
+  const [selected, setSelected] = useState(1);
 
-  if (account) {
-    return (
+  const options: any = {
+    1: (<a href="https://chrome.google.com/webstore/detail/argent-x/dlcobpjiigpikoobohmabehhmhfoodbb">install argent x</a>),
+    2: (
       <div>
-        <span>connected: {shortenAddress(account)} </span>
+        <span>connected: {shortenAddress(account!)} </span>
         <button onClick={() => disconnect()}>
           disconnect
         </button>
       </div>
+    ),
+    3: (
+      <div>
+        {available.map((connector) => (
+          <li className='list-none' key={connector.id()}>
+            <button onClick={() => connect(connector)}>
+              {`connect ${connector.name().toLowerCase()}`}
+            </button>
+          </li>
+        ))}
+      </div>
     )
   }
 
-  if (available.length === 0) { // ask the user to install argent x if no wallet is found
-    return <a href="https://chrome.google.com/webstore/detail/argent-x/dlcobpjiigpikoobohmabehhmhfoodbb">install argent x</a>
-  }
+  useEffect(() => {
+
+  }, [available, account])
 
   return (
     <div>
-      {available.map((connector) => (
-        <li className='list-none' key={connector.id()}>
-          <button onClick={() => connect(connector)}>
-            {`connect ${connector.name().toLowerCase()}`}
-          </button>
-        </li>
-      ))}
+      {options[selected]}
     </div>
   )
 }
